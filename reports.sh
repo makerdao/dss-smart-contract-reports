a9d1e562532da912af6a55c35e90751a2d928b10
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-07-24 14:50:21 -0600

    wip trying to regex out the color codes

--- reports.sh
+++ reports.sh
@@ -15,25 +15,38 @@ CONTRACTS=$DSS_DIR/src/*.sol
 
 for c in $CONTRACTS
 do
-	echo "$c"
 	NAME=$(basename "${c%.sol}")
 	echo $NAME
 	mkdir "${OUT_DIR}/${NAME}"
 	surya describe "$DSS_DIR/src/${NAME}.sol" > ${OUT_DIR}/${NAME}/describe_${NAME}.txt
 	describe="${OUT_DIR}/${NAME}/describe_${NAME}.txt"
-	currentContract=""
+	CONTRACT=""
 	while IFS= read -r line
 	do
-		ar=($line)
-		echo ${ar[0]}
-		echo ${ar[1]}
-		if [ "${ar[0]}" == "+" ]; then
-			contractName=${ar[1]}
-			surya dependencies ${contractName} "$DSS_DIR/src/${NAME}.sol" > ${OUT_DIR}/${NAME}/dependencies_${contractName}_${NAME}.txt
-		else
-			echo "not contract ${ar}"	
-		fi
-		
+			set -ex
+		line=$(sed 's/#//g' <<< "$line")
+		echo -e $line
+		# line=$(sed "s,\[[0-9;]*m,,g" <<< "$line")
+		line=$(sed 's/\x1B\[[0-9;]*m//g' <<< "$line")
+		echo -e $line
+		# line=${line//"#[39m"/}
+		# line=${line//"[31m"/}
+		# line=${line//"[32m"/}
+		# line=${line//"[39m"/}
+		# ar=($line)
+		# echo ${ar[*]}
+		# if [ "${ar[0]}" == "+" ]; then
+		# 	CONTRACT=${ar[1]}
+		# 	surya dependencies ${CONTRACT} "$DSS_DIR/src/${NAME}.sol" > ${OUT_DIR}/${NAME}/dependencies_${CONTRACT}_${NAME}.txt
+		# elif [ "${ar[0]}" == "-" ]; then
+		# 	set -x
+		# 	echo "function ${ar[2]}"
+		# 	surya ftrace ${CONTRACT}::${ar[2]} all "$DSS_DIR/src/$NAME.sol" > $OUT_DIR/$NAME/ftrace_${CONTRACT}_${ar[2]}_${NAME}.txt
+		# 	set +x
+		# else
+		# 	echo "neither ${ar[0]}"
+		# fi
+
 	done < "$describe"
 	surya inheritance $c | dot -Tpng > "${OUT_DIR}/${NAME}/inheritance_${NAME}.png"
 	surya graph $c | dot -Tpng > "${OUT_DIR}/${NAME}/graph_${NAME}.png"
