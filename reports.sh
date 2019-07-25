defb70af77017714f6189e28fff0e3ca4febb9d7
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-07-25 15:55:58 -0600

    remove color codes from txt files

--- reports.sh
+++ reports.sh
@@ -10,7 +10,7 @@ if [ -z "${DSS_DIR}" ]; then
 	DSS_DIR="../dss"
 fi
 
-surya describe $DSS_DIR/**/*.sol > ${OUT_DIR}/dss-describe.txt
+surya describe $DSS_DIR/**/*.sol | sed -E 's/[[:cntrl:]]\[[0-9;]*m//g' > ${OUT_DIR}/dss-describe.txt
 surya graph $DSS_DIR/**/*.sol | dot -Tpng > ${OUT_DIR}/dss-graph.png
 
 CONTRACTS=$DSS_DIR/src/*.sol
@@ -20,22 +20,22 @@ do
 	NAME=$(basename "${c%.sol}")
 	echo "Starting Contract: ${NAME}"
 	mkdir "${OUT_DIR}/${NAME}"
-	surya describe "$DSS_DIR/src/${NAME}.sol" > ${OUT_DIR}/${NAME}/describe_${NAME}.txt
+	surya describe "$DSS_DIR/src/${NAME}.sol" | sed -E 's/[[:cntrl:]]\[[0-9;]*m//g' > ${OUT_DIR}/${NAME}/describe_${NAME}.txt
 	describe="${OUT_DIR}/${NAME}/describe_${NAME}.txt"
 	CONTRACT=""
 	while IFS= read -r line
 	do
 		line=$(sed 's/#//g' <<< "$line")
-		line=$(sed -E 's/[[:cntrl:]]\[[0-9;]*m//g' <<< "$line")
+		# line=$(sed -E 's/[[:cntrl:]]\[[0-9;]*m//g' <<< "$line")
 		ar=($line)
 		# echo ${ar[*]}
 		if [ "${ar[0]}" == "+" ]; then
 			CONTRACT="${ar[1]}"
 			echo "dependencies of ${CONTRACT}"
-			surya dependencies "${CONTRACT}" "${DSS_DIR}/src/${NAME}.sol" > ${OUT_DIR}/${NAME}/dependencies_${CONTRACT}_${NAME}.txt
+			surya dependencies "${CONTRACT}" "${DSS_DIR}/src/${NAME}.sol" | sed -E 's/[[:cntrl:]]\[[0-9;]*m//g' > ${OUT_DIR}/${NAME}/dependencies_${CONTRACT}_${NAME}.txt
 		elif [ "${ar[0]}" == "-" ]; then
 			echo "ftrace of ${ar[2]} for ${CONTRACT}"
-			surya ftrace "${CONTRACT}::${ar[2]}" all "${DSS_DIR}/src/${NAME}.sol" > "${OUT_DIR}/${NAME}/ftrace_${CONTRACT}_${ar[2]}_${NAME}.txt"
+			surya ftrace "${CONTRACT}::${ar[2]}" all "${DSS_DIR}/src/${NAME}.sol" | sed -E 's/[[:cntrl:]]\[[0-9;]*m//g' > "${OUT_DIR}/${NAME}/ftrace_${CONTRACT}_${ar[2]}_${NAME}.txt"
 		fi
 
 	done < "$describe"
